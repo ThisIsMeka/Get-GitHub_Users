f9f0cdc52f06def96f9b5529bc37ddef47d75bac
Meerim Abdinazarova <Meerim0814@gmail.com>
2020-05-20 13:38:21 -0400

    header edited

--- client/components/headers.js
+++ client/components/headers.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Header = (props) => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-800g">
+    <nav className="flex items-center justify-between flex-wrap bg-gray-800">
       <div className="flex items-center text-white p-0 mr-0 0 0 0">
         {props.userName && <img className="w-12 mx-2" src={props.user.avatar_url} alt="avatar" />}
         {props.userName && <span className="text-xl mx-2">{props.user.login}</span>}
